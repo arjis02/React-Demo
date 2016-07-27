27955bfabdc17d927f2c011712a998f8e51d2cdf
arjis02 <rj.ramos@gmail.com>
2016-07-27 18:26:36 -0400

    Create components for dashboard

--- mobile-dashboard.js
+++ mobile-dashboard.js
@@ -8,7 +8,7 @@ var btnAddJob = React.DOM.button({
   children: "Add Job"
 });
 
-var btnAllJob = React.DOM.button({
+var btnAllJobs = React.DOM.button({
   className: "btn btn-md btn-info",
   children: "All Job"
 });
@@ -20,7 +20,7 @@ var btnAllExpenses = React.DOM.button({
 
 var btnAllPayments = React.DOM.button({
   className: "btn btn-md btn-info",
-  children: "All Expenses"
+  children: "All Payments"
 });
 
 var btnAddExpense = React.DOM.button({
@@ -28,11 +28,63 @@ var btnAddExpense = React.DOM.button({
   children: "Add Expense"
 });
 
+var InviteComponent = React.createClass({
+  render: function() {
+    var numOfTenants = 2;
+
+    return (
+      <div className="component">
+        {btnInvite}
+        <p>Current tenants: ({numOfTenants})</p>
+      </div>
+    );
+  }
+});
+
+var ChoresComponent = React.createClass({
+  render: function() {
+    var currentChore = "Throw garbage out";
+
+    return (
+      <div className="component">
+        <label>Upcoming chore:</label>
+        <h3>{currentChore}</h3>
+        {btnAddJob}
+        {btnAllJobs}
+      </div>
+    );
+  }
+});
+
+var BalancesComponent = React.createClass({
+  render: function() {
+    var balances = [
+    {name: "John", amount: "owes you: $5.75"},
+    {name: "Peter", amount: "owes you: $6.80"}
+    ];
+
+    return (
+      <div className="component">
+        <label>Balances:</label>
+        <ul>
+          { balances.map( user => <li key={user.name}>{user.name} {user.amount}</li> ) }
+        </ul>
+        {btnAllExpenses}
+        {btnAddExpense}
+        {btnAllPayments}
+      </div>
+    );
+  }
+});
+
 var Dashboard = React.createClass({
   render: function() {
     return (
       <div>
         <h1>rTab Dashboard</h1>
+        <InviteComponent />
+        <ChoresComponent />
+        <BalancesComponent />
       </div>
     );
   }
